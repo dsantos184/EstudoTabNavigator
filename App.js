13f0f432f900b2bf6cbc86864cfd9c7c9ec6d6cd
dsantos184 <41877514+dsantos184@users.noreply.github.com>
2018-10-17 15:58:36 -0300

    TabNavigation + StackNavigation
    Utilizando duas navegações diferentes

--- App.js
+++ App.js
@@ -5,20 +5,46 @@
  */
 
 import React, { Component } from 'react';
+import {Image} from 'react-native';
+
 import { TabNavigator } from 'react-navigation';
 
 import Principal from './src/pages/Principal';
 import Conversa from './src/pages/Conversa'
+import Config from './src/pages/Config'
 
 const Navegador = TabNavigator({
   Home:
   {
     screen: Principal
   },
-  Conversa:
+  Conversa: {
+    screen:Conversa,
+    navigationOptions: 
+    {
+      title:"Conversas",
+      tabBarLabel:"Conversas",
+      tabBarIcon:({tintColor, focused}) => {
+        if(focused) 
+        {
+			    return (
+            <Image source={require("./src/assets/img/chat_on.png")} style={{width:26, height:26}} />
+			   );
+        } 
+        else 
+        {
+			    return (
+				    <Image source={require("./src/assets/img/chat_off.png")} style={{width:26, height:26}} />
+			    );
+			  }					
+      }
+    }    
+  },
+  Config:
   {
-    screen: Conversa
+    screen: Config
   }
+
 },{
   tabBarOptions:
   {
